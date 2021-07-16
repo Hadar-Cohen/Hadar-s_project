e168c4288911ee4f14f5c90d9e37f773940fa6cc
hadartayar <hadar6611@gmail.com>
2021-07-16 23:20:50 +0300

    ht

--- finalServerSide/Scripts/viewChat.js
+++ finalServerSide/Scripts/viewChat.js
@@ -13,7 +13,7 @@ function modal() {
     btn.onclick = function () {
         modal.style.display = "block";
     }
-
+     
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
